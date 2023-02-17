26e04cf76a1dbe47a1d21cd58a850f1b1be46f1c
Zhouxy5279 <1145733074@qq.com>
2023-02-17 10:11:28 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# HelloWorld.py
+# 记录一些python特性的使用方法
+
+# file_script
+一些常用的文件操作脚本
---
+++ file_script/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# Author      - Zhouxy5279
+# Email       - zhouxy5279@qq.com
+# Time        - 2023/2/17
+# Description -
